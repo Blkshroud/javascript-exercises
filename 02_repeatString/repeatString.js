82d225a5947f89a10013cfa4a7354239db7086c9
Blkshroud <145509002+Blkshroud@users.noreply.github.com>
2023-10-23 22:04:59 -0400

    Completed Exercise 2 NOTE: Don't set variables in an if statement when you want to use equals... === is your friend.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringRepeat = string;
+        for(let i = 1; i<+num; i++){
+            stringRepeat += string;
+        }
+    if(num < 0){
+        stringRepeat = 'ERROR';
+    }
+    else if(num === 0){
+        stringRepeat = '';
+    }
+    return stringRepeat
 };
 
 // Do not edit below this line
