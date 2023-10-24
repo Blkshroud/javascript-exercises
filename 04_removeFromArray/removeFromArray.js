883ffd6dc65bbebb0e00f886ed388f9d872b24b1
Blkshroud <145509002+Blkshroud@users.noreply.github.com>
2023-10-24 18:24:03 -0400

    Completed Exercise 4 Used two loops to define the array and argument counts to cycle through and compare each argument against each position in the array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    let array = arguments[0];
+    for(let argPos = 1;argPos < arguments.length; argPos++){        
+        for(let arrayPos = 0; arrayPos < array.length;arrayPos++){
+            if(array[arrayPos] === arguments[argPos]){
+                array.splice(arrayPos,1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
