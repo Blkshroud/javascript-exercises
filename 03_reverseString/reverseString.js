3df0d09f65914b568833b02abf311b0455a8bfbc
Blkshroud <145509002+Blkshroud@users.noreply.github.com>
2023-10-24 16:30:27 -0400

    Completed Exercise 3 Used a for loop to reorder with concatenation. Solution used a combination of string and array methods.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    if(word === ''){
+        return word;
+    }
+    let subWord = '';
+    for(let i = word.length;i>0;i--){
+        subWord += word.substring(i,i-1);
+    }
+    return subWord;
 };
 
 // Do not edit below this line
