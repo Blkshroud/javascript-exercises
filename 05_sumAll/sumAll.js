359f227c5fa55b0a10d720928cb54045f403711a
Blkshroud <145509002+Blkshroud@users.noreply.github.com>
2023-10-25 17:51:28 -0400

    Completed Exercise 5 My code is likely more confusing to someone else, so it should be cleaned up if it weren't for an exercise.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let smallerNum = 0;
+    let biggerNum = 0;
+    if(typeof num1 != 'number' || typeof num2 != 'number' || num1 < 0 || num2 < 0){
+        return 'ERROR';
+    }
+    else if(num1 > num2){
+        biggerNum = num1;
+        smallerNum = num2;
+    }
+    else{
+        biggerNum = num2;
+        smallerNum = num1;
+    }
+    let sum = smallerNum;
+    for(let i = smallerNum+1; i <= biggerNum; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
