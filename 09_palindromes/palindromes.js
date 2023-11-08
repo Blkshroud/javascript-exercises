0a764ab2e3c2cf8431695663795842d7c25a3d82
Blkshroud <145509002+Blkshroud@users.noreply.github.com>
2023-11-08 18:12:15 -0500

    Completed Exercise 9 Example solution used two lines and chained functions to compare forward and reversed strings. I created multiple variables and ran individual character comparisons via for loop. Attempted to increase solving speed by stopping comparison halfway. Running the test suite against both indicated no difference in speed. Speeds were similar for solution and example solution as well.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (palCheck) {
+    let isPal = true;
+    palCheck = palCheck.toLowerCase();
+    palCheck = palCheck.replace(/[^a-z0-9]/g, "");
+    let endNum = palCheck.length-1;
+    let palLength = endNum;
+    for(let startNum = 0; startNum<=palLength; startNum++){
+        let palStart = palCheck.substr(startNum, 1);
+        let palEnd = palCheck.substr(endNum, 1);
+        if(palStart != palEnd){
+            isPal = false;
+        }
+        endNum--;
+    }
+    return isPal;
 };
 
 // Do not edit below this line
