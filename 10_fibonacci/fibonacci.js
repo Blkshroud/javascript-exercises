52020a12a92d7cae69ae2ee0f67f569dcb708e0d
Blkshroud <145509002+Blkshroud@users.noreply.github.com>
2023-11-08 18:25:26 -0500

    Completed Exercise 10 My solution was nearly identical to example solution.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibPos) {
+    if(fibPos < 0){
+        return "OOPS";
+    }
+    +fibPos;
+    let pos1 = 1;
+    let pos2 = 0;
+    let temp = 0;
+    for(let i = 0; i<fibPos;i++){
+        temp = pos1;
+        pos1 += pos2;
+        pos2 = temp;
+    }
+    return temp;
 };
 
 // Do not edit below this line
