57c9b146295a91142c5bce53827170ba266d2758
Blkshroud <145509002+Blkshroud@users.noreply.github.com>
2023-11-09 16:05:40 -0500

    Completed Exercise 12 Typoes are the enemy of good code. -yearofDeath instead of yearOfDeath -getFullYear instead of getFullYear()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people.sort(function (personA, personB){
+        if(typeof personA.yearOfDeath == 'undefined' || personA.yearOfDeath == null){
+            personA.yearOfDeath = new Date().getFullYear();
+        }
+        if(typeof personB.yearOfDeath == 'undefined' || personB.yearOfDeath == null){
+            personB.yearOfDeath = new Date().getFullYear();
+        }
+        return  (personB.yearOfDeath - personB.yearOfBirth) - (personA.yearOfDeath - personA.yearOfBirth);
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
